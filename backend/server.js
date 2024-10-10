51892011239871c90f7ddbcd565ae71018e77bc2
Aarush Kumar <98913930+Aarushbgs@users.noreply.github.com>
2024-10-10 12:29:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,12 @@ const Attendance= require('./Models/Attendance');
 require('./Models/db');
 require('dotenv').config();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://cook-maid-services-ui.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 
 
