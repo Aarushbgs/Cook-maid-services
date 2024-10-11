f10968caf02134945f7455288a7324906199fdab
Aarush Kumar <98913930+Aarushbgs@users.noreply.github.com>
2024-10-11 12:48:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,20 +5,17 @@ const bodyParser = require('body-parser');
 const cors= require('cors');
 
 const corsOptions = {
-  origin: 'https://cook-maid-services-ui.vercel.app',  // Add your frontend's origin here
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Specify the allowed methods
-  credentials: true  // Allow cookies or authentication headers
+  origin: ['https://cook-maid-services-ui.vercel.app', 'https://cook-maid-services-qspn.vercel.app'],  
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  
+  credentials: true  
 };
+
 
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://cook-maid-services-ui.vercel.app');  // Specify your frontend's origin
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+
 
 const AuthRouter= require('./Routes/AuthRouter');
 const findroute= require('./Routes/worker');
